a33a1f63d2629fedcf7b1fb195e356786b3d49bf
Juan Miguel de Haro Ruiz <juan.deharoruiz@bsc.es>
2024-06-11 15:42:22 +0200

    Fix xtasks test

--- test/emu/xtasks/simple.c
+++ test/emu/xtasks/simple.c
@@ -10,6 +10,7 @@ int
 main(void)
 {
 	instr_start(0, 1);
+	instr_require("xtasks");
 
 	/* Example trace:
 	 * 69539385   +69539385  Fse :00:00:00:00:00:00:00:00:4e:00:00:00:b0:ae:19:15
@@ -20,9 +21,9 @@ main(void)
 	 * 69540615        +200  Fse :01:00:00:00:00:00:00:00:4f:00:00:00:b0:ae:19:15
 	 */
 
-	instr_xtasks_ev(0, 0x4e, 123);
+	instr_xtasks_ev(123, 0x4e, 0);
 	sleep_us(100);
-	instr_xtasks_ev(1, 0x4e, 123);
+	instr_xtasks_ev(123, 0x4e, 1);
 
 	instr_end();
 
